582807cdf84a4df5a845793410a17f5d59f022a1
PanzXx <145233819+PannSH@users.noreply.github.com>
2023-09-17 11:35:53 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -25,7 +25,7 @@ LimitApikey = 200 // Limit Apikey default
 
 //___________ ┏  Database Mongodb Setting  ┓ ___________\\
 
-keymongodb = process.env.mongodb "mongodb+srv://pansh:bu0johzUWDK5m9Qv@pansh.yn2wgy4.mongodb.net/?retryWrites=true&w=majority"; //example "mongodb+srv://user:password@name_database.mongodb.net/?retryWrites=true&w=majority";
+keymongodb = process.env.mongodb //example "mongodb+srv://user:password@name_database.mongodb.net/?retryWrites=true&w=majority";
 
 //___________ ┏  SMTP Setting  ┓ ___________\\
 
